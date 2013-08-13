4023b0ee75351e84164632d06b3570992bad6bc5
Christian Seidel <sh3xbeer@gmail.com>
2013-08-13 17:33:46 +0200

    some validation changes

--- json2rss.php
+++ json2rss.php
@@ -1,15 +1,18 @@
 <?php
 require_once("url.php");
 
+header('Content-Type: application/rss+xml');
+
 $content = file_get_contents($url);
 $json = json_decode($content);
-var_dump($json);
+#var_dump($json);
 
 $xml = new DOMDocument('1.0', 'UTF-8');
 $xml->formatOutput = true;
 
 $roo = $xml->createElement('rss');
 $roo->setAttribute('version', '2.0');
+$roo->setAttribute("xmlns:atom","http://www.w3.org/2005/Atom");
 $xml->appendChild($roo); 
 
 $cha = $xml->createElement('channel');
@@ -34,7 +37,13 @@ $cha->appendChild($hea);
 #$hea = $xml->createElement('link',htmlentities('http://xml-rss.de')); 
 #$cha->appendChild($hea);
 
-$hea = $xml->createElement("link", htmlentities("http://test.com"));
+$hea = $xml->createElement("link", htmlentities("http://webcomic.stefanie-hoefig.de"));
+$cha->appendChild($hea);
+
+$hea = $xml->createElement("atom:link");
+$hea->setAttribute("href",  htmlentities("http://shexbeer.square7.ch/json2rss.php"));
+$hea->setAttribute("rel", "self");
+$hea->setAttribute("type", "application/rss+xml");
 $cha->appendChild($hea);
 
 
@@ -42,30 +51,29 @@ $hea = $xml->createElement('lastBuildDate',
 utf8_encode(date("D, j M Y H:i:s ").'GMT'));
 $cha->appendChild($hea);
 
-
-for ($i=0; $i < 2; $i++) { 
+foreach ($json as $key => $value) {
 	$itm = $xml->createElement('item');
     $cha->appendChild($itm);
 
 	$dat = $xml->createElement('title',
-	utf8_encode('Titel der Nachricht')); 
+	$value->headline); 
 	$itm->appendChild($dat);
 
 
 	$dat = $xml->createElement('description',
-	utf8_encode('Die Nachricht an sich')); 
+	utf8_encode('Neues Kapitel in Manga '+$value->manga+" von "+$value->author+" gepostet.")); 
 	$itm->appendChild($dat);    
 
 	$dat = $xml->createElement('link',
-	htmlentities('http://www.test.de'));
+	htmlentities($value->url));
 	$itm->appendChild($dat);
 
 	$dat = $xml->createElement('pubDate',
-	utf8_encode('Datum der Nachricht')); 
+	utf8_encode($value->date)); 
 	$itm->appendChild($dat);
 
 	$dat = $xml->createElement('guid',
-	htmlentities('Einzigartige ID')); 
+	$value->id); 
 	$itm->appendChild($dat);
 }
 
